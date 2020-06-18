c0cc7b907a6911bb54c8488ec35d82ebc2160f00
d-mjlee <audwn0326@gmail.com>
2020-06-18 21:46:34 +0900

    done

--- app.js
+++ app.js
@@ -218,15 +218,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
     // setup app
-    // ui.setupAPP();
+    ui.setupAPP();
     
     // get all products
-    // products.getProducts().then(products => {
-    //     ui.displayProducts(products);
-    //     Storage.saveProducts(products);
-    // }).then(() => {
-    //     ui.getBagButtons();
-    //     ui.cartLogic();
-    // });
+    products.getProducts().then(products => {
+        ui.displayProducts(products);
+        Storage.saveProducts(products);
+    }).then(() => {
+        ui.getBagButtons();
+        ui.cartLogic();
+    });
 
 })
